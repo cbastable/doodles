d76dc292daf2cc087a10bdfd0b336c463f67aa85
cbastable <therightstuff17@gmail.com>
2014-01-29 00:33:42 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Doodles::Application.config.secret_key_base = 'd7e458f17320d22db01dc28399c62c644c80a1c8db0834cb7f5995e0b2adba0055874d87c1f74059db195e508b31ac2d0e1e2e202bc76536dcfe433078157c83'
