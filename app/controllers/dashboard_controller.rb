2c369f03271cdfb3bb3319539b7d2cfa316b9136
cbastable <therightstuff17@gmail.com>
2014-02-05 22:30:31 -0500

    Simple burn calc

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,7 @@ class DashboardController < ApplicationController
 	def home
 	end
 
-	
+	def simple_burn_calc
+		
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Doodles::Application.routes.draw do
   resources :posts
 
   root to: 'dashboard#home'
+  get "/burn", to: "dashboard#simple_burn_calc", as: :burn
 end
